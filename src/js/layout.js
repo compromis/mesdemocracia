bbcc5f5e575fdf624061c110d974ba86f77fdc2c
Guillem <guillem@disedit.com>
2018-02-08 18:13:55 +0100

    Update page background

--- src/js/layout.js
+++ src/js/layout.js
@@ -11,7 +11,7 @@ import Footer from './components/footer';
 class Layout extends React.Component {
   render() {
     return (
-      <div>
+      <div className="wrapper">
         <Header />
         <Hero />
         <div className="main-block">
