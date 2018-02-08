ac804a92f661571a599c37fa349553446434822c
Guillem <guillem@disedit.com>
2018-02-08 16:59:30 +0100

    Add temp placeholder

--- src/js/layout.js
+++ src/js/layout.js
@@ -4,20 +4,26 @@ import { translate } from "react-translate";
 
 import Header from './components/header';
 import Hero from './components/hero';
+import Placeholder from './components/placeholder';
 import Progress from './components/progress';
 import Footer from './components/footer';
 
 class Layout extends React.Component {
-
   render() {
-
     return (
       <div>
         <Header />
         <Hero />
         <div className="main-block">
           <div className="container" id="content">
-            <Progress />
+            <div className="row">
+              <div className="col-md-6">
+                <Placeholder />
+              </div>
+              <div className="col-md-6">
+                <Progress />
+              </div>
+            </div>
           </div>
         </div>
         <Footer />
